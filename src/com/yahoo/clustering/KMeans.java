f9653af371f75a5ea551b0ab73cd36fb43fbc3e2
Antonio Severien <severien@yahoo-inc.com>
2013-03-18 19:44:11 +0100

    finished simple version of kmeans

--- src/com/yahoo/clustering/KMeans.java
+++ src/com/yahoo/clustering/KMeans.java
@@ -8,13 +8,8 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
 import java.util.StringTokenizer;
-import java.util.TreeMap;
 
 /**
  * k-means clustering algorithm
@@ -25,74 +20,85 @@ import java.util.TreeMap;
 public class KMeans {
 
 	File dataset;
-	List<List<Double>> points;
 	int kClusters;
 	int dimension;
+	int dataSetSize = 20;
+	List<List<Double>> points;
 	List<List<Double>> clustersCentroids;
 	List<List<Integer>> clustersMapping;
-	int dataSetSize=20;
 
 	public KMeans() {
 		kClusters = 3;
 		dimension = 2;
 		points = new ArrayList<List<Double>>(dataSetSize);
-		//clusters = new HashSet<List<Double>>(kClusters);
 		clustersCentroids = new ArrayList<List<Double>>(kClusters);
-		// Key=N, Value=clusterID
 		clustersMapping = new ArrayList<List<Integer>>(kClusters);
-		for (int i=0 ; i<kClusters ; i++) {
-			clustersMapping.add(i, new ArrayList<Integer>());	
+		for (int i = 0; i < kClusters; i++) {
+			clustersMapping.add(i, new ArrayList<Integer>());
 		}
-		
-		
+	}
+	
+	public KMeans(int clusters, int dimension) {
+		this.kClusters = clusters;
+		this.dimension = dimension;
+		this.points = new ArrayList<List<Double>>(dataSetSize);
+		this.clustersCentroids = new ArrayList<List<Double>>(kClusters);
+		this.clustersMapping = new ArrayList<List<Integer>>(kClusters);
+		for (int i = 0; i < kClusters; i++) {
+			clustersMapping.add(i, new ArrayList<Integer>());
+		}
 	}
 
+	// TODO optimize kmeans algorithm for better performance and memory usage
 	protected void run() {
-		// TODO kmeans algorithm
 		setKRandomPoints();
-		// objective function does not converge do
+		// until objective function does not converge... do
 		euclideanDistance(points, clustersCentroids);
 		// loop while convergence of objective function not satisfied
 		// calculate the new centroid points
-	//	while (!isStopCondition()) {
-//		for(int i=0;i<10;i++){
-//			euclideanDistance(points, clustersCentroids);
-//		}
-	}
-	
-	protected List<Double> getNewCentroid(int clusterId) {
-		List<Double> newCentroid = new ArrayList<>(dimension);
-		List<Integer> pointsIndex = clustersMapping.get(clusterId);
-		double sum = 0.0d;
-		for (int d = 0; d < dimension; d++) {
-			for (int i=0; i< pointsIndex.size(); i++) {
-				//List<Double> point = points.get(pointsIndex.get(i)).get(d);
-				// for each dimension get the mean
-				sum += points.get(pointsIndex.get(i)).get(d);
+		List<Double> newCentroid = new ArrayList<Double>(dimension);
+		int pointsCounter = 0;
+		double sum = 0d;
+		boolean isStopCondition = false;
+		while (!isStopCondition) {
+			for (int clusterID = 0; clusterID < clustersMapping.size(); clusterID++) {
+				for (int d = 0; d < dimension; d++) {
+					List<Integer> indexList = clustersMapping.get(clusterID);
+					for (Integer clusterPoint : indexList) {
+						sum += points.get(clusterPoint).get(d);
+						pointsCounter++;
+					}
+					newCentroid.add(d, sum / pointsCounter);
+					sum = 0;
+					pointsCounter = 0;
+				}
+				if (!compare(clustersCentroids.get(clusterID), newCentroid)) {
+					clustersCentroids.set(clusterID, newCentroid);
+					isStopCondition = false;
+				} else {
+					isStopCondition = true;
+				}
+				newCentroid = new ArrayList<Double>(dimension);
 			}
-			newCentroid.add(d, sum/dimension);
+			System.out.println(clustersCentroids.get(0));
+			System.out.println(clustersCentroids.get(1));
+			System.out.println(clustersCentroids.get(2));
+
+			for (int i = 0; i < kClusters; i++) {
+				clustersMapping.get(i).clear();
+			}
+
+			euclideanDistance(points, clustersCentroids);
 		}
-		return newCentroid;
 	}
-	
-	protected boolean isStopCondition() {
-		boolean isStop = true;
-		int i=0;
-		List<Double> newCentroid;
-		while(isStop) {
-			// if new centroid is the same as old centroid
-			newCentroid = getNewCentroid(i);
-			if (!clustersCentroids.get(i).equals(newCentroid)) {
-				isStop = false;
-				clustersCentroids.set(i, newCentroid);
-			} 
+
+	protected boolean compare(List<Double> p1, List<Double> p2) {
+		boolean isEqual = true;
+		for (int i = 0; i < dimension; i++) {
+			isEqual = (p1.get(i).doubleValue() == p2.get(i).doubleValue()) ? true
+					: false;
 		}
-		return isStop;
-	}
-	
-	protected boolean comparePoints(List<Double> oldCentroid, List<Double> newCentroid) {
-		//oldCentroid.e
-		return true;
+		return isEqual;
 	}
 
 	/*
@@ -105,6 +111,7 @@ public class KMeans {
 		int indexC = 0;
 		double minDistance = 0.0d;
 		double distance = 0.0d;
+
 		// iterate over all points N
 		for (int n = 0; n < x.size(); n++) {
 			// iterate over centroids C
@@ -127,28 +134,37 @@ public class KMeans {
 				}
 				sum = 0.0d;
 			}
+			// this is adding duplicate values, check it !!
 			clustersMapping.get(indexC).add(n);
-			indexC = 0;			
+			indexC = 0;
 		}
 		this.print();
 
 	}
 
+	/*
+	 * TODO Search for methods on picking a good seed
+	 */
 	protected void setKRandomPoints() {
-		//for (int i = 0; i < kClusters; i++) {
-		//	clustersCentroids.add(points.get((int) (Math.random() * dataSetSize)));
-			
-		//	System.out.println(clusters.toString());
-		//}
-		clustersCentroids.add(0,points.get(18));
-		clustersCentroids.add(1,points.get(19));
-		clustersCentroids.add(2,points.get(4));
-		System.out.println("Centroids 0:\n"+points.get(18).get(0)+","+points.get(18).get(1));
-		System.out.println("Centroids 1:\n"+points.get(19).get(0)+","+points.get(19).get(1));
-		System.out.println("Centroids 2:\n"+points.get(10).get(0)+","+points.get(10).get(1));
+		// for (int i = 0; i < kClusters; i++) {
+		// clustersCentroids.add(points.get((int) (Math.random() *
+		// dataSetSize)));
+
+		// System.out.println(clusters.toString());
+		// }
+		clustersCentroids.add(0, points.get(18));
+		clustersCentroids.add(1, points.get(19));
+		clustersCentroids.add(2, points.get(4));
+		System.out.println("Centroids 0:\n" + points.get(18).get(0) + ","
+				+ points.get(18).get(1));
+		System.out.println("Centroids 1:\n" + points.get(19).get(0) + ","
+				+ points.get(19).get(1));
+		System.out.println("Centroids 2:\n" + points.get(10).get(0) + ","
+				+ points.get(10).get(1));
 	}
-	
+
 	// generates dataset points list of 3 dimension
+	// TODO read points from dataset...
 	protected void generateData() {
 
 		for (int i = 0; i < dataSetSize; i++) {
@@ -162,18 +178,21 @@ public class KMeans {
 		}
 	}
 
+	// TODO Print function to plot on a graph... gnuplot??
 	protected void print() {
-		for (int i=0; i<clustersMapping.size() ; i++) {
+		for (int i = 0; i < clustersMapping.size(); i++) {
 			for (Integer index : clustersMapping.get(i)) {
-				//System.out.print("["+i+"="+index+"]");
-				System.out.print(i+",");
-				System.out.println(points.get(index).get(0)+","+points.get(index).get(1));
+				System.out.print(i + ",");
+				System.out.println(points.get(index).get(0) + ","
+						+ points.get(index).get(1));
 			}
-			//System.out.println(" * ");
 		}
 		System.out.println("\n----------------------------\n");
 	}
-	
+
+	/*
+	 * Writes generated dataset to file
+	 */
 	protected void writeDataSet() {
 		File f = new File(System.getProperty("user.dir")
 				+ "/resources/dataset.txt");
@@ -193,12 +212,14 @@ public class KMeans {
 			bw.write(sb.toString());
 			bw.close();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
 	}
 
+	/*
+	 * Reads dataset from file
+	 */
 	protected void readDataSet() {
 		File f = new File(System.getProperty("user.dir")
 				+ "/resources/dataset.txt");
@@ -209,7 +230,7 @@ public class KMeans {
 			BufferedReader br = new BufferedReader(fr);
 			String line;
 			int skipCount = 0;
-			while ((line = br.readLine()) != null ) {
+			while ((line = br.readLine()) != null) {
 				skipCount++;
 				if (skipCount > 40) {
 					StringTokenizer st = new StringTokenizer(line, ",");
@@ -224,27 +245,21 @@ public class KMeans {
 				if (skipCount - 40 == dataSetSize)
 					break;
 			}
-			
+
 			br.close();
 			fr.close();
 		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (NumberFormatException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-
-	
 
 	public static void main(String[] args) {
 		KMeans kmeans = new KMeans();
 		kmeans.readDataSet();
 		kmeans.run();
-		//kmeans.print();
 	}
 }
